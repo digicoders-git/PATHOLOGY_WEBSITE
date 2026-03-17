409b4427be39ba00e49a16e4d536e2abc8fb6e46
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-03-17 12:10:30 +0530

    loader removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import appRoute from "./routes/appRoute";
 import FixedAppDownload from "./components/FixedAppDownload";
-import Loader from "./components/Loader";
 
 const App = () => {
-  const location = useLocation();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const timer = setTimeout(() => setLoading(false), 600);
-    return () => clearTimeout(timer);
-  }, [location.pathname]);
-
   return (
     <div>
-      {loading && <Loader />}
       <Navbar />
       <FixedAppDownload />
       <Routes>
